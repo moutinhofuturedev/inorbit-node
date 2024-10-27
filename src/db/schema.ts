74d944d123c32a0531b7e5eb96bbba4192059ad3
paulo <paulo_vicali@icloud.com>
2024-10-27 01:22:15 -0300

    feat(v2): cria tabela de usuários para autenticação

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,23 @@
 import { createId } from '@paralleldrive/cuid2'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
+export const users = pgTable('users', {
+  id: text('id')
+    .primaryKey()
+    .$default(() => createId()),
+  name: text('name'),
+  email: text('email'),
+  avatarUrl: text('avatar_url').notNull(),
+  externalAccountId: text('external_account_id').notNull().unique(),
+})
+
 export const goals = pgTable('goals', {
   id: text('id')
     .primaryKey()
     .$default(() => createId()),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull(),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
